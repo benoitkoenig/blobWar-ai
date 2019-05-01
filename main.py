07e1e7c374762ec1d89701141591d9c4de4c2bb4
Benoit Koenig <koenigbenoit47@gmail.com>
2019-05-01 11:13:20 +0400

    Initial commit from the reinforcement learning with linear function approximation that previously was in the blobwar main repo

---
+++ main.py
@@ -0,0 +1,35 @@
+import socketio
+import os
+
+from agent import Agent
+
+sio = socketio.Client()
+agents = {}
+
+@sio.on('connect')
+def on_connect():
+    print('[Python] SocketIO connected: AI is up. Identifying as the AI')
+    sio.emit('connectAI', {'passwd': 'Kore wa watashi no passwd'})
+
+@sio.on('create_learning_agent')
+def on_create_learning_agent(data):
+    agents[data["id"]] = Agent(sio, data["id"])
+
+@sio.on('action')
+def on_action(data):
+    agents[data["agentId"]].action(data["data"])
+
+@sio.on('terminate_agent')
+def on_terminate_agent(data):
+    del agents[data["agentId"]]
+
+@sio.on('disconnect')
+def on_disconnect():
+    print('[Python] SocketIO disconnected: closing AI')
+
+address = "http://localhost:8080"
+if hasattr(os.environ, "ENV"):
+    if os.environ["ENV"] == "heroku":
+        address = "http://blobwar.herokuapp.com/"
+
+sio.connect(address)
