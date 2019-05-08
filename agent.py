d6b7dfd4bc39588faca0cd1cc22474822325572f
Benoit Koenig <koenigbenoit47@gmail.com>
2019-05-09 00:56:30 +0400

    Updating Ws. Still a bit unsatisfactory, Victory rate is 0.73, below previous values of 0.80

--- agent.py
+++ agent.py
@@ -20,8 +20,8 @@ print("Loading Ws")
 for key in Ws:
     print(key, Ws[key].min(), Ws[key].max())
 
-alpha = 1. / W_SIZE
-epsilon = 0.1
+alpha = .1 / W_SIZE
+epsilon = 0.001
 gamma = 0.9
 traceDecay = 0.9
 
