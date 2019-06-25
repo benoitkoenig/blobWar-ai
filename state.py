cc6f2b108a539882c4fcda9673d692984baca588
Benoit Koenig <koenigbenoit47@gmail.com>
2019-06-25 09:39:05 -0600

    Re-organize state.py and write all unit tests for it

--- state.py
+++ state.py
@@ -23,9 +23,8 @@ def get_angle(b1, b2):
 def get_distance(b1, b2):
     return math.sqrt(((b1["x"] - b2["x"]) ** 2 + (b1["y"] - b2["y"]) ** 2) / 2) # divided by 2 so the max distance is 1
 
-def get_state_vector(state, name):
+def get_cards_data(state):
     state_vector = [
-        1,
         int(state["cards"]["availability"][0]),
         int(state["cards"]["availability"][1]),
     ]
@@ -34,6 +33,10 @@ def get_state_vector(state, name):
             int(state["cards"]["currentBlob"][0] == i),
             int(state["cards"]["currentBlob"][1] == i),
         ]
+    return state_vector
+
+def get_all_blobs_features(state):
+    state_vector = []
     all_blobs = (state["army"] + state["enemy"])
     for blob in all_blobs:
         state_vector += [
@@ -44,6 +47,11 @@ def get_state_vector(state, name):
             blob["x"],
             blob["y"],
         ]
+    return state_vector
+
+def get_pair_blob_features(state):
+    state_vector = []
+    all_blobs = (state["army"] + state["enemy"])
     for i in range(len(all_blobs)):
         for j in range(i):
             b1 = all_blobs[i]
@@ -51,14 +59,37 @@ def get_state_vector(state, name):
             state_vector += [
                 get_distance(b1, b2),
                 get_angle(b1, b2),
+                get_angle(b2, b1),
             ]
+    return state_vector
+
+def get_angle_between_three_blobs(state):
+    state_vector = []
+    all_blobs = (state["army"] + state["enemy"])
+    for i in range(len(all_blobs)):
+        for j in range(len(all_blobs)):
             for k in range(j):
-                combinations = [[i, j, k], [j, k, i], [k, i, j]]
-                for c in combinations:
-                    b1 = all_blobs[c[0]]
-                    b2 = all_blobs[c[1]]
-                    b3 = all_blobs[c[2]]
+                if (i != j) & (i != k):
+                    b1 = all_blobs[i]
+                    b2 = all_blobs[j]
+                    b3 = all_blobs[k]
                     state_vector.append(get_angle(b1, b2) - get_angle(b1, b3))
+    return state_vector
+
+def get_in_range_data(state, name):
+    state_vector = []
+    for blob in state["army"]:
+        for other_blob in state["enemy"]:
+            if name in ranges:
+                distance = get_distance(blob, other_blob)
+                in_range = int(distance <= ranges[name])
+            else:
+                in_range = 0
+            state_vector.append(in_range)
+    return state_vector
+
+def get_scalar_data(state):
+    state_vector = []
     for blob in state["army"]:
         if (blob["destination"] == None):
             destination_vector = [0, 0]
@@ -67,10 +98,15 @@ def get_state_vector(state, name):
         for other_blob in state["enemy"]:
             to_other_blob_vector = [other_blob["x"] - blob["x"], other_blob["y"] - blob["y"]]
             scalar = get_scalar(destination_vector, to_other_blob_vector)
-            if name in ranges:
-                distance = get_distance(blob, other_blob)
-                in_range = int(distance <= ranges[name])
-            else:
-                in_range = 0
-            state_vector += [in_range, scalar]
+            state_vector.append(scalar)
+    return state_vector
+
+def get_state_vector(state, name):
+    state_vector = [1]
+    state_vector += get_cards_data(state)
+    state_vector += get_all_blobs_features(state)
+    state_vector += get_pair_blob_features(state)
+    state_vector += get_angle_between_three_blobs(state)
+    state_vector += get_in_range_data(state, name)
+    state_vector += get_scalar_data(state)
     return state_vector
--- tests/test_state.py
+++ tests/test_state.py
@@ -22,27 +22,29 @@ def generate_state():
         "army": [
             generate_blob(.2, .1, 1),
             generate_blob(.5, .1, 1),
-            generate_blob(.9, .1, 1),
+            generate_blob(.8, .1, 1),
         ],
         "enemy": [
             generate_blob(.2, .9, 1),
             generate_blob(.5, .9, 1),
-            generate_blob(.9, .9, 1),
+            generate_blob(.8, .9, 1),
         ]
     }
 
 class TestStateUtilityMethods(unittest.TestCase):
     def test_get_scalar(self):
+        v0 = [0, 0]
         v1 = [0, 1]
         v2 = [1, 2]
-        scalar = state.get_scalar(v1, v2)
-        self.assertEqual(scalar, 2 / math.sqrt(5))
+        self.assertEqual(state.get_scalar(v0, v2), 0)
+        self.assertEqual(state.get_scalar(v1, v2), 2 / math.sqrt(5))
 
     def test_get_angle(self):
         b1 = {"x": 0, "y": 1}
         b2 = {"x": 1, "y": 2}
-        angle = state.get_angle(b1, b2)
-        self.assertAlmostEqual(angle, math.pi / 4)
+        b3 = {"x": 1, "y": 0}
+        self.assertAlmostEqual(state.get_angle(b1, b2), math.pi / 4)
+        self.assertAlmostEqual(state.get_angle(b1, b3), -math.pi / 4)
 
     def test_get_distance(self):
         b1 = {"x": 0, "y": 1}
@@ -50,25 +52,103 @@ class TestStateUtilityMethods(unittest.TestCase):
         dist = state.get_distance(b1, b2)
         self.assertEqual(dist, 1)
 
-class TestStateGetStateVector(unittest.TestCase):
-    def test_1_constant(self):
+class TestStateGetStateMethods(unittest.TestCase):
+    def test_get_cards_data(self):
         s = generate_state()
-        state_vector = state.get_state_vector(s, "")
-        self.assertEquals(state_vector[0], 1)
-
-    def test_cards_availability(self):
-        s = generate_state()
-        state_vector = state.get_state_vector(s, "")
-        self.assertEqual(state_vector[1], True)
-        self.assertEqual(state_vector[2], True)
         s["cards"]["availability"][0] = False
+        s["cards"]["currentBlob"][0] = 1
+        state_vector = state.get_cards_data(s)
+        self.assertEqual(state_vector, [0, 1, 0, 0, 1, 0, 0, 0])
+
+    def test_get_all_blobs_features(self):
+        s = generate_state()
+        s["army"][0] = generate_blob(.2, .1, 1, alive=False)
+        s["army"][1] = generate_blob(.5, .1, 1, status="hat")
+        s["army"][2] = generate_blob(.8, .1, 1, status="ghost")
+        s["enemy"][0] = generate_blob(.2, .9, 3, status="hat")
+        state_vector = state.get_all_blobs_features(s)
+        self.assertEqual(state_vector[0:6], [0, 1, 0, 0, .2, .1])
+        self.assertEqual(state_vector[6:12], [1, 0, 0, 1, .5, .1])
+        self.assertEqual(state_vector[12:18], [1, 0, 1, 0, .8, .1])
+        self.assertEqual(state_vector[18:24], [1, 0, 0, 1, .2, .9])
+        self.assertEqual(state_vector[24:30], [1, 1, 0, 0, .5, .9])
+        self.assertEqual(state_vector[30:36], [1, 1, 0, 0, .8, .9])
+
+    def test_get_pair_blob_features(self):
+        s = generate_state()
+        s["enemy"][0] = generate_blob(.2, .4, 3)
+        s["enemy"][1] = generate_blob(.5, .4, 3)
+        s["enemy"][2] = generate_blob(.8, .4, 3)
+        state_vector = state.get_pair_blob_features(s)
+        # 1, 0
+        self.assertAlmostEqual(state_vector[0], .3 / math.sqrt(2))
+        self.assertAlmostEqual(state_vector[1], math.pi)
+        self.assertAlmostEqual(state_vector[2], 0)
+        # 2, 1
+        self.assertAlmostEqual(state_vector[6], .3 / math.sqrt(2))
+        self.assertAlmostEqual(state_vector[7], math.pi)
+        self.assertAlmostEqual(state_vector[8], 0)
+        # 3, 0
+        self.assertAlmostEqual(state_vector[9], .3 / math.sqrt(2))
+        self.assertAlmostEqual(state_vector[10], -math.pi / 2)
+        self.assertAlmostEqual(state_vector[11], math.pi / 2)
+        # 4, 2
+        self.assertAlmostEqual(state_vector[24], .3)
+        self.assertAlmostEqual(state_vector[25], - math.pi / 4)
+        self.assertAlmostEqual(state_vector[26], 3 * math.pi / 4)
+        # 5, 0
+        self.assertAlmostEqual(state_vector[30], math.sqrt(.45 / 2))
+        self.assertAlmostEqual(state_vector[31], -2.677945044588987)
+        self.assertAlmostEqual(state_vector[32], math.pi - 2.677945044588987)
+        # 5, 4
+        self.assertAlmostEqual(state_vector[42], .3 / math.sqrt(2))
+        self.assertAlmostEqual(state_vector[43], math.pi)
+        self.assertAlmostEqual(state_vector[44], 0)
+
+    def test_get_angle_between_three_blobs(self):
+        s = generate_state()
+        s["enemy"][0] = generate_blob(.2, .4, 3)
+        s["enemy"][1] = generate_blob(.5, .4, 3)
+        s["enemy"][2] = generate_blob(.8, .4, 3)
+        state_vector = state.get_angle_between_three_blobs(s)
+        # 0, 2, 1
+        self.assertAlmostEqual(state_vector[0], 0)
+        # 1, 2, 0
+        self.assertAlmostEqual(state_vector[10], -math.pi)
+        # 2, 1, 0
+        self.assertAlmostEqual(state_vector[20], 0)
+        # 3, 1, 0
+        self.assertAlmostEqual(state_vector[30], math.pi / 4)
+        # 4, 1, 0
+        self.assertAlmostEqual(state_vector[40], math.pi / 4)
+        # 5, 1, 0
+        self.assertAlmostEqual(state_vector[50], 0.32175055439664213)
+
+    def test_in_range_data(self):
+        s = generate_state()
+        s["enemy"][0] = generate_blob(.1, .1, 3)
+        s["enemy"][1] = generate_blob(.5, .1, 3, status="ghost")
+        state_vector = state.get_in_range_data(s, "RBotGhostBloc")
+        self.assertEqual(state_vector, [0, 0, 0, 0, 1, 0, 0, 0, 0])
+        state_vector = state.get_in_range_data(s, "RBotDashDash")
+        self.assertEqual(state_vector, [1, 0, 0, 0, 1, 0, 0, 0, 0])
+        state_vector = state.get_in_range_data(s, "ThisOneDoesNotExist")
+        self.assertEqual(state_vector, [0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+    def test_get_scalar_data(self):
+        s = generate_state()
+        s["army"][0] = generate_blob(.2, .1, 1, destination={"x": .2, "y": .9})
+        s["enemy"][0] = generate_blob(.2, .4, 3)
+        s["enemy"][1] = generate_blob(.5, .4, 3)
+        s["enemy"][2] = generate_blob(.8, .4, 3)
+        state_vector = state.get_scalar_data(s)
+        self.assertEqual(state_vector, [1, 1 / math.sqrt(2), 1 / math.sqrt(5), 0, 0, 0, 0, 0, 0])
+
+    def test_state_get_state_vector(self):
+        s = generate_state()
         state_vector = state.get_state_vector(s, "")
-        self.assertEqual(state_vector[1], False)
-        self.assertEqual(state_vector[2], True)
-        s["cards"]["availability"][1] = False
-        state_vector = state.get_state_vector(s, "")
-        self.assertEqual(state_vector[1], False)
-        self.assertEqual(state_vector[2], False)
+        self.assertEqual(len(state_vector), 168)
+        self.assertEqual(state_vector[0], 1)
 
 if __name__ == "__main__":
     unittest.main()
