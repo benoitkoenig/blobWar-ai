e63034b0efbc85f83172dee93cb4e2e532b07311
Benoit Koenig <koenigbenoit47@gmail.com>
2019-06-25 12:22:14 -0600

    Update constants

--- constants.py
+++ constants.py
@@ -1,15 +1,15 @@
 import math
 
-STATE_SIZE = 153
+STATE_SIZE = 168
 ACTION_SIZE = 55
 
 step_size = 4
 gamma = .9
 epsilon = .1
-prob_flattener_factor = .005
+prob_flattener_factor = .01
 update_interval = 10
-learning_rate_actor = 1e-3
-learning_rate_critic = 1e-3
+learning_rate_actor = 1e-5
+learning_rate_critic = 1e-5
 
 names = ["RBotGhostBloc", "RBotDashDash"]
 
